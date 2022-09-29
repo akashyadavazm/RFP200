385bfc80c1c5fce2bb71b73a4280449eb1550686
akashyadavazm <akashyadavazm@gmail.com>
2022-09-29 18:44:20 +0530

    Day5

---
+++ Day5Problems/VowelOrConsonant.java
@@ -0,0 +1,20 @@
+package Day5Problems;
+
+public class VowelOrConsonant {
+	public static void main(String[] args) {
+
+		char ch = 'z';
+
+		if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+
+			System.out.println(ch + " is Vowel");
+
+		} else {
+
+			System.out.println(ch + " is Consonant");
+
+		}
+
+	}
+
+}
