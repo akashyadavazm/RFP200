7fd65cf6f6de65b8aec5a90f38fbbadb6c8ffaba
akashyadavazm <akashyadavazm@gmail.com>
2022-09-29 18:34:50 +0530

    Day4

---
+++ day4Problems/EmployeeWages/Display.java
@@ -0,0 +1,12 @@
+package day4Problems.EmployeeWages;
+
+public class Display {
+	public static void displayWelcome() {
+		System.out.println("Display Welcome to employees wage computation");
+	}
+	
+public static void main(String[] args) {
+	displayWelcome();
+	}
+
+}
