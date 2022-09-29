a34797d7b96c2e5d6b2efd9ffb6d6f0141c2379c
akashyadavazm <akashyadavazm@gmail.com>
2022-09-29 18:07:16 +0530

    Day3

---
+++ PrintElement.java
@@ -0,0 +1,15 @@
+package Day3Problem;
+
+public class PrintElement {
+
+	public static void main(String[] args) {
+		
+		int Array[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		
+		for (int i = 0; i < Array.length; i++) {
+			System.out.println(Array[i]);
+		}
+
+	}
+
+}
