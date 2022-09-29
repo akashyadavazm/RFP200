185cf7eea7b0bcae04fdd9e843a48befb78f7911
akashyadavazm <akashyadavazm@gmail.com>
2022-09-29 18:20:42 +0530

    Line Comparison

---
+++ DisplayWelcome.java
@@ -0,0 +1,10 @@
+package Day3.linecomparisonproblem;
+
+public class DisplayWelcome {
+	public static void LineComparison() {
+		System.out.println("Welcome");
+	}
+public static void main(String[] args) {
+	LineComparison();
+	}
+}
