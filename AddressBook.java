8f48c65def5f22d59919195e638351aa436281ca
Akash Yadav <109862673+akashyadavazm@users.noreply.github.com>
2022-11-05 02:02:01 +0530

    Day10Problems

---
+++ AddressBook.java
@@ -0,0 +1,75 @@
+package day10Problems;
+
+public class AddressBook {
+	String Email = null;
+	String LastName = null;
+	String FirstName = null;
+	String address, city, state;
+	int zip;
+	long phoneNo;
+
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+
+	public String getLastName() {
+		return LastName;
+	}
+
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+
+	public String getFirstName() {
+		return FirstName;
+	}
+
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	public long getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(long phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+}
---
+++ OperateContact.java
@@ -0,0 +1,25 @@
+package day10Problems;
+
+public class OperateContact {
+
+	public void storeContact(String firstName, AddressBook contact) {
+	
+		
+	}
+
+	public void editContact() {
+		
+		
+	}
+
+	public void showContact() {
+		
+		
+	}
+
+	public void deleteContact() {
+		
+		
+	}
+
+}
